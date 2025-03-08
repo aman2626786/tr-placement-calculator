a8479f63f50023147575c2b885e7b027fd0c9733
AMAN SHARMA <159331028+aman2626786@users.noreply.github.com>
2025-03-09 01:04:20 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Install required system-level dependencies
-pip install --upgrade pip
-
-# Install dependencies without build isolation to use pre-built wheels
-pip install --no-build-isolation -r requirements.txt
